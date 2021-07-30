a18c70ae809d9f2165119c6c351c55200a4ec39c
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-07-30 22:13:53 +0300

    Spawn update for pattern

--- Asteroid Race/Assets/Scripts/Spawn/SpawnItems.cs
+++ Asteroid Race/Assets/Scripts/Spawn/SpawnItems.cs
@@ -4,6 +4,7 @@ public class SpawnItems : MonoBehaviour
 {
     [SerializeField] private GameObject itemPrefab;
     [SerializeField] private bool active;
+
     public bool Active
     {
         get { return active; }
@@ -11,9 +12,8 @@ public class SpawnItems : MonoBehaviour
     }
 
     [SerializeField] private float timerWait = 0.0f;
-    [SerializeField] private GameObject dronePlayer;
-    private GameObject _item;
 
+    private GameObject _item;
     private float timer = 0.0f;
 
 
@@ -26,16 +26,12 @@ public class SpawnItems : MonoBehaviour
 
     private void Spawn()
     {
-
-
         if (timer >= Random.Range(1.5f, 3.0f))
         {
             _item = Instantiate(itemPrefab) as GameObject;
-            _item.transform.position = new Vector3(Random.Range(-2.3f, 2.3f), 6.4f, 0);
-
 
             timer = timerWait;
         }
-        timer += Time.deltaTime; 
+        timer += Time.deltaTime;
     }
 }
