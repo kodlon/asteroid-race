ed57b1190d1516cb3d766d63c98ed1f2fb926935
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-07-31 00:58:50 +0300

    Added armor bonus

---
+++ Asteroid Race/Assets/Scripts/Bonus/ArmorBonus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ArmorBonus : MonoBehaviour, IBonuses
+{
+    public IEnumerator BonusCorutine(GameObject playerObject, SpriteRenderer gunDrone, Animator bonusPanel, SpriteRenderer playerSprite)
+    {
+        playerObject.GetComponent<Player>().Health++;
+        playerSprite.color = new Color(1f, 0.1686275f, 0.1686275f, 1.0f);
+        bonusPanel.SetInteger("ChangeAnim", 1);
+
+        yield break;
+    }
+}
